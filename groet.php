4401e53b8ab9c236392eb725fa833c3b14fc230d
BOUDEWIJN BOUT <2089175@talnet.nl>
2020-04-02 13:30:59 +0200

    Uitwerking van de exercise

---
+++ groet.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <!-- Meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- Title -->
+    <title>Wie is daar?</title>
+</head>
+
+<!-- Content -->
+<body>
+    
+    <!-- H1 - Main -->
+    <h1>Schrijf Hallo Computer</h1>
+
+    <!-- Main Form -->
+    <form method="post">
+        <input type="text" name="number_1">
+
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
+
+<!-- PHP Start -->
+<?php
+
+// Submit click check
+
+if (isset($_POST['submit'])) {
+
+    $server_reaction = $_POST['number_1'];
+
+    if ($server_reaction == "Hallo Computer") {
+        echo "Hallo Gebruiker!";
+    }
+
+    if ($server_reaction == "hallo computer") {
+        echo "Hallo Gebruiker!";
+    }
+}
+
+// PHP End
+?>
